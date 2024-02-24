075a4293e9e8a5bcfe0d7e5de9587f8b269c21e8
tgrgao <tigerygao@gmail.com>
2024-02-23 19:12:00 -0500

    Basic queue functionality now works

--- file-mq.cpp
+++ file-mq.cpp
@@ -5,6 +5,34 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+ssize_t get_file_size(int fd) {
+    struct stat st;
+    fstat(fd, &st);
+    return st.st_size;
+}
+
+void print_metadata_bytes(int fd) {
+    ssize_t file_size = get_file_size(fd);
+
+    // Ensure the file size is a multiple of 4
+    if (file_size % 4 != 0) {
+        std::cout <<  "File size is not a multiple of 4 bytes.\n";
+        return;
+    }
+
+    // Rewind the file pointer to the beginning
+    lseek(fd, 0, SEEK_SET) == -1;
+
+    // Read and process the file in chunks of 4 bytes
+    uint8_t buffer[4];
+    int i = 0;
+    while (read(fd, buffer, sizeof(buffer)) == sizeof(buffer)) {
+        int value = (buffer[3] << 24) | (buffer[2] << 16) | (buffer[1] << 8) | buffer[0];
+        printf("%d: %d\n", i, value);
+        i += 4;
+    }
+}
+
 enum EntryStatus {
     READY,
     UNACK,
@@ -53,12 +81,6 @@ int write_metadata_entry(int fd, off_t addr, struct MetadataEntry *entry) {
     return 0;
 }
 
-ssize_t get_file_size(int fd) {
-    struct stat st;
-    fstat(fd, &st);
-    return st.st_size;
-}
-
 int lock_file(int fd, struct flock *lock_ptr) {
     // fcntl(fd, F_SETLKW, lock_ptr);
     return 0;
@@ -79,6 +101,13 @@ FileMQ::FileMQ(std::string queue_dir_path) {
         std::memset(zeros, 0, METADATA_START);
         fd_meta = open(file_path_chars, O_CREAT | O_RDWR);
         write(fd_meta, zeros, METADATA_START);
+
+        lseek(fd_meta, METADATA_READY_PTR_ADDR, SEEK_SET);
+        unsigned metadata_start = METADATA_START;
+        write(fd_meta, &metadata_start, sizeof(metadata_start));
+
+        lseek(fd_meta, METADATA_UNACK_PTR_ADDR, SEEK_SET);
+        write(fd_meta, &metadata_start, sizeof(metadata_start));
     }
 
     free(file_path_chars);
@@ -109,7 +138,7 @@ int FileMQ::enqueue(void *buf, unsigned *id, size_t size) {
     // make metadata entry and write at end of metadata file
     MetadataEntry metadata_entry; 
     metadata_entry.id = *id;
-    metadata_entry.data_ptr = get_file_size(fd_meta);
+    metadata_entry.data_ptr = get_file_size(fd_data);
     metadata_entry.data_size = size;
     metadata_entry.status = EntryStatus::READY;
     write_metadata_entry(fd_meta, get_file_size(fd_meta), &metadata_entry);
@@ -135,16 +164,17 @@ int FileMQ::enqueue(void *buf, unsigned *id, size_t size) {
 int FileMQ::dequeue(void *buf, unsigned *id, size_t *size, size_t max_size) {
     lock_file(fd_meta, &metadata_lock);
 
-    off_t ready_ptr;
-    read_unsigned(fd_meta, METADATA_READY_PTR_ADDR, (unsigned *) &ready_ptr);
+    unsigned ready_ptr;
+    read_unsigned(fd_meta, METADATA_READY_PTR_ADDR, &ready_ptr);
 
     if (ready_ptr == get_file_size(fd_meta)) {
         return -1; // nothing ready
     }
-
+    
     MetadataEntry metadata_entry;
     lseek(fd_meta, ready_ptr, SEEK_SET);
     read(fd_meta, &metadata_entry, sizeof(metadata_entry));
+
     *id = metadata_entry.id;
 
     if (metadata_entry.status != EntryStatus::READY) {
@@ -166,11 +196,19 @@ int FileMQ::dequeue(void *buf, unsigned *id, size_t *size, size_t max_size) {
 
     // increment unack count
     unsigned unack_count;
-    lseek(fd_meta, METADATA_READY_COUNT_ADDR, SEEK_SET);
+    lseek(fd_meta, METADATA_UNACK_COUNT_ADDR, SEEK_SET);
     read(fd_meta, &unack_count, sizeof(unack_count));
     ++unack_count;
-    lseek(fd_meta, METADATA_READY_COUNT_ADDR, SEEK_SET);
+    lseek(fd_meta, METADATA_UNACK_COUNT_ADDR, SEEK_SET);
     write(fd_meta, &unack_count, sizeof(unack_count));
+
+    // decrement ready count
+    unsigned ready_count;
+    lseek(fd_meta, METADATA_READY_COUNT_ADDR, SEEK_SET);
+    read(fd_meta, &ready_count, sizeof(ready_count));
+    --ready_count;
+    lseek(fd_meta, METADATA_READY_COUNT_ADDR, SEEK_SET);
+    write(fd_meta, &ready_count, sizeof(ready_count));
 
     // advance ready pointer
     ready_ptr += sizeof(metadata_entry);
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,8 @@
 #include "../file-mq.h"
 #include <format>
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main(int argc, char *argv[]) {
     // if (argc != 2) {
@@ -13,11 +15,16 @@ int main(int argc, char *argv[]) {
     char buf[10] = "Hello";
     unsigned id;
     ret = myQ.enqueue(buf, &id, 10);
+    strcpy(buf, "world!");
+    ret = myQ.enqueue(buf, &id, 10);
 
-    std::cout << ret << " " << "Enqueue id" << id << std::endl;
+    // std::cout << ret << " " << "Enqueue id " << id << std::endl;
 
     char buf2[10];
     size_t size;
-    myQ.dequeue(buf2, &id, &size, 10);
-    std::cout << ret << " "  << "Dequeue id" << id << " " << buf2 << std::endl;
+    ret = myQ.dequeue(buf2, &id, &size, 10);
+    std::cout << buf2 << std::endl;
+    ret = myQ.dequeue(buf2, &id, &size, 10);
+    std::cout << buf2 << std::endl;
+    // std::cout << ret << " "  << "Dequeue id " << id << " " << buf2 << std::endl;
 }
