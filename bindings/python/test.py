802850eb954ddceb1e3af2d60e3cf5529fc55967
tgrgao <tigerygao@gmail.com>
2024-03-19 20:54:50 -0400

    Added benchmark for python binding tests, and some debug code. Lock failure still happens on fcntl call due to 'Invalid argument'

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -1,15 +1,25 @@
 import filemq
+import time
 
 queue = filemq.FileMQ()
 
-queue.init("./test-queue")
+result = queue.init("./test-queue")
+
+if result == filemq.Result.FAILURE:
+    quit()
 
 test_string = "Hello world!"
 
-result = queue.enqueue(test_string,  len(test_string))
+start_time = time.time()
 
-print(result)
+for i in range(100000):
+    result = queue.enqueue(test_string, len(test_string))
 
-result = queue.dequeue(len(test_string))
+for i in range(100000):
+    result = queue.dequeue(len(test_string))
+    id = result[1]
+    result = queue.ack(id)
 
-print(result)
+end_time = time.time()
+
+print(end_time - start_time)
