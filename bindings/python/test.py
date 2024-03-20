2f80b831a2e643d3138b23f6005ba1d0c9a272a9
tgrgao <tigerygao@gmail.com>
2024-03-19 20:38:22 -0400

    Python bindings for all basic ack queue functions work. Sometimes, acquiring file lock for python is intermittent, do not know why

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -3,3 +3,13 @@ import filemq
 queue = filemq.FileMQ()
 
 queue.init("./test-queue")
+
+test_string = "Hello world!"
+
+result = queue.enqueue(test_string,  len(test_string))
+
+print(result)
+
+result = queue.dequeue(len(test_string))
+
+print(result)
